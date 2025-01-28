322510b65a87aa59b0305034731d46eda2665b4b
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2025-01-28 17:52:34 +0700

    Transaksi: Menambahkan ViewModel dan State untuk Insert Transaksi yang mencangkup InsertTssUiState, InsertTssUiTransaksi, serta konversi antara model Transaksi dan state UI untuk penanganan input transaksi pada aplikasi

---
+++ app/src/main/java/com/example/a13finalucp/ui/viewmodel/transaksi/InsertTransaksiViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.a13finalucp.ui.viewmodel.transaksi
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.a13finalucp.model.Transaksi
+import com.example.a13finalucp.repository.TransaksiRepository
+import kotlinx.coroutines.launch
+
+
+
+data class InsertTssUiState(
+    val insertTssUiTransaksi: InsertTssUiTransaksi = InsertTssUiTransaksi()
+)
+
+data class InsertTssUiTransaksi(
+    val id_transaksi: Int = 0,
+    val id_tiket: Int = 0,
+    val jumlah_tiket: Int = 0,
+    val jumlah_pembayaran: Int = 0,
+    val tanggal_transaksi: String = "",
+)
+
+fun InsertTssUiTransaksi.toTss(): Transaksi = Transaksi(
+    id_transaksi = id_transaksi,
+    id_tiket = id_tiket,
+    jumlah_tiket = jumlah_tiket,
+    jumlah_pembayaran = jumlah_pembayaran,
+    tanggal_transaksi = tanggal_transaksi
+)
+
+fun Transaksi.toUiStateTss(): InsertTssUiState = InsertTssUiState(
+    insertTssUiTransaksi = toInsertTssUiTransaksi()
+)
+
+fun Transaksi.toInsertTssUiTransaksi(): InsertTssUiTransaksi = InsertTssUiTransaksi(
+    id_transaksi = id_transaksi,
+    id_tiket = id_tiket,
+    jumlah_tiket = jumlah_tiket,
+    jumlah_pembayaran = jumlah_pembayaran,
+    tanggal_transaksi = tanggal_transaksi
+)
