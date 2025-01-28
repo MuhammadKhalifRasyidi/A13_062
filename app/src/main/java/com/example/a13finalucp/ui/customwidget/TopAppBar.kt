f3cb4243e8658d8c630b7fcb3b24f0acc97377a5
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2025-01-28 16:37:32 +0700

    menambahkan CostumeTopAppBar dengan judul, navigasi kembali, dan aksi refresh

---
+++ app/src/main/java/com/example/a13finalucp/ui/customwidget/TopAppBar.kt
@@ -0,0 +1,75 @@
+package com.example.a13finalucp.ui.customwidget
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CostumeTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {},
+    onRefresh: () -> Unit = {}
+) {
+    val gradientBrush = Brush.linearGradient(
+        colors = listOf(
+            Color(0xFF0055A4), // Biru
+            Color(0xFFB22234)  // Merah
+        )
+    )
+
+    Box(
+        modifier = Modifier
+            .background(gradientBrush)
+    ) {
+        CenterAlignedTopAppBar(
+            title = {
+                Text(title,
+                color = Color.White // Warna teks putih
+            ) },
+            actions = {
+                Icon(
+                    imageVector = Icons.Default.Refresh,
+                    contentDescription = "",
+                    tint = Color.White,
+                    modifier = Modifier
+                        .clickable {
+                            onRefresh()
+                    }
+                )
+            },
+            modifier = modifier,
+            scrollBehavior = scrollBehavior,
+            colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                containerColor = Color.Transparent // Transparan agar gradien terlihat
+            ),
+            navigationIcon = {
+                if (canNavigateBack) {
+                    IconButton(onClick = navigateUp) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            tint = Color.White,
+                            contentDescription = null)
+                    }
+                }
+            }
+        )
+    }
+}
