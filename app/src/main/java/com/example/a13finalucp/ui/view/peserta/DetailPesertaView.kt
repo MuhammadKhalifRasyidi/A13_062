bdb5462e7e762a15e387839c67c2c11b1dbc9f13
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2025-01-28 17:13:01 +0700

    Peserta: Menambahkan komponen baris detail untuk menampilkan label dan nilai

---
+++ app/src/main/java/com/example/a13finalucp/ui/view/peserta/DetailPesertaView.kt
@@ -0,0 +1,64 @@
+package com.example.a13finalucp.ui.view.peserta
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.a13finalucp.ui.customwidget.CostumeTopAppBar
+import com.example.a13finalucp.ui.navigation.DestinasiNavigasi
+import com.example.a13finalucp.ui.view.transaksi.DestinasiHomeTransaksi
+import com.example.a13finalucp.ui.viewmodel.peserta.DetailPesertaViewModel
+import com.example.a13finalucp.ui.viewmodel.peserta.PenyediaPesertaViewModel
+
+object DestinasiDetailPeserta : DestinasiNavigasi {
+    override val route = "detailPeserta"
+    override val titleRes = "Detail Peserta"
+    const val IDPeserta = "id_peserta"
+    val routeWithArgs = "$route/{$IDPeserta}"
+}
+
+
+
+@Composable
+fun DetailPstRow(label: String, value: Any) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = label,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.weight(1f)
+        )
+        Text(
+            text = ": $value",
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.weight(2f)
+        )
+    }
+}
