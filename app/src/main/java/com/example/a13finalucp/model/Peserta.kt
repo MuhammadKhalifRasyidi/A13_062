828e2e02d20ef45df787743b36248f57eb7139b7
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2025-01-28 15:10:49 +0700

    Peserta: menambahkan model Peserta dan response untuk data peserta

---
+++ app/src/main/java/com/example/a13finalucp/model/Peserta.kt
@@ -0,0 +1,25 @@
+package com.example.a13finalucp.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Peserta (
+    val id_peserta: Int = 0,
+    val nama_peserta: String,
+    val email: String,
+    val nomor_telepon: String
+)
+
+@Serializable
+data class PesertaResponse(
+    val status: Boolean,
+    val message: String,
+    val data: List<Peserta>
+)
+
+@Serializable
+data class PesertaResponseDetail(
+    val status: Boolean,
+    val message: String,
+    val data: Peserta
+)
