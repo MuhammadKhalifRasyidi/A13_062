59070869559b4a46f54b89602a90bcc9407aef7b
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2025-01-28 15:26:51 +0700

    Peserta: menambahkan extension function untuk mengonversi model Peserta menjadi InsertPstUiPeserta

---
+++ app/src/main/java/com/example/a13finalucp/ui/viewmodel/peserta/InsertPesertaViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.a13finalucp.ui.viewmodel.peserta
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.a13finalucp.model.Peserta
+import com.example.a13finalucp.repository.PesertaRepository
+import kotlinx.coroutines.launch
+
+
+
+fun Peserta.toInsertPstUiPeserta(): InsertPstUiPeserta = InsertPstUiPeserta(
+    id_peserta = id_peserta,
+    nama_peserta = nama_peserta,
+    email = email,
+    nomor_telepon = nomor_telepon
+)
